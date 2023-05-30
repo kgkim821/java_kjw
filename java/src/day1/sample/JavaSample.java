3069831dfdd870feb8c5d9951c9a19f6d5686215
kgkim821 <kgkim821@naver.com>
2023-05-30 15:07:12 +0900

    java 변수 기본형 및 선언 방법 예제

--- java/src/day1/sample/JavaSample.java
+++ java/src/day1/sample/JavaSample.java
@@ -2,10 +2,22 @@ package day1.sample;
 
 public class JavaSample {
 
+	// 한줄 주석 : 코드에 영향이 없는 설명 문구
+	/* 여러줄주석
+	 */
+	/**
+	 * 메소드를 설명하는 주석
+	 * @param args
+	 */
+	
+	//main : 프로그램이 실행되기 위해 필요한 메소드
+	
+	
 	public static void main(String[] args) {
+		//콘솔창에 원하는 문구를 출력하는 기능
 		System.out.println("Hello Java!");
-		
 		
 	}
 
 }
+
