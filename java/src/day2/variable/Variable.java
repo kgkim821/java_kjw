ae9acf89fc78c82fe4c58ebfd90e0f84eb54c119
kgkim821 <kgkim821@naver.com>
2023-05-31 12:41:47 +0900

    java 변수 선언 예제

---
+++ java/src/day2/variable/Variable.java
@@ -0,0 +1,13 @@
+package day2.variable;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		int num1 = 10 ;
+		//e는 10 을 의미 , e-3 은 10의 -3제곱을의미 , 결과는 double 형
+		double num2 = 3e-3;//3/1000 == 0.003
+
+		System.out.println(num2);
+	}
+
+}
