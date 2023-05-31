145feb7ed13f13a06c2e3d879b4b1019d5168a0d
kgkim821 <kgkim821@naver.com>
2023-05-31 12:41:33 +0900

    java 연습문제

---
+++ java/src/day2/variable/Practice.java
@@ -0,0 +1,19 @@
+package day2.variable;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		
+		// 1번 문제
+		// 문자는 '' 로 감싸줘야된다 "" 로 감싸주면 안됨
+		// char ch1 = 'A';
+		
+		//2번 문제
+		//국어 영어 수학 성적을 입력하여 평균을 구하려 한다. 이때 필요한 변수들을 선언하세요.
+		
+		int kor, eng, math;
+		double avg;
+				
+	}
+
+}
