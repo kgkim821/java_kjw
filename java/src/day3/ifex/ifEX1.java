5b721c3203aaf0e56f03e40a928cdf373cd8328d
kgkim821 <kgkim821@naver.com>
2023-06-01 14:57:31 +0900

    if 문예제

---
+++ java/src/day3/ifex/ifEX1.java
@@ -0,0 +1,24 @@
+package day3.ifex;
+
+public class ifEX1 {
+
+	public static void main(String[] args) {
+			//정수 num가 0 이면 0 입니다라고 출력하는 코드를 작성하세요.
+			//정수 num가 0 이다.
+		//정수 num가 0 과 같다.
+		int num =10;
+		if( num == 0) {
+			//0입니다라고 출력;
+			System.out.println("0 is 0");
+		}
+		//정수 num 가 아니면 num is not 0 라고 출력하는 코드를 작성하세요.
+		//num 가 0 이 아니다 .
+		//num 가 0 과 같지 않다.
+		if(num != 0)
+		{
+			System.out.println(num + " is not 0");
+		}
+		
+	}
+
+}
