6c2d49124c7d54a1b7939a20450197906b211088
kgkim821 <kgkim821@naver.com>
2023-06-01 13:35:11 +0900

    자바 2회자 과제풀이

--- java/src/day2/homewaork/Ex1.java
+++ java/src/day2/homewaork/Ex1.java
@@ -1,10 +1,44 @@
 package day2.homewaork;
 
+import java.util.Scanner;
+
 public class Ex1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+			
+		
+		
+		
+		int num1, num2;
+		char operator;
+		Scanner scan;
+			
+		
+		Scanner sc = new Scanner(System.in);
+				
+			System.out.println("두 정수와 산술연산자를 입력 하세요");
 
+			num1=sc.nextInt();
+			operator=sc.next().charAt(0);
+			num2=sc.nextInt();
+			System.out.println(""+ num1+ " " + operator +" "+ num2);
+			
+			
+			//String str1 = sc.next(); // 스페이스를 포함한 문자열 한줄을 읽어옴(마지막 입력한 엔터 포함)
+			//System.out.println(str1);
+			
+			sc.close();
+		
+		
 	}
 
 }
+
+
+/*콘솔에서 정수 2개와 문자 (산술연산자)를 입력받아 출력하는 코드를 작성하세요
+
+예시
+
+두 정수와 산술연산자를 입력 하세요 (예 1+2) : 
+[입력] 1+2
+[출력] 1+2*/
--- java/src/day2/homewaork/Ex2.java
+++ java/src/day2/homewaork/Ex2.java
@@ -1,10 +1,78 @@
 package day2.homewaork;
 
+import java.util.Scanner;
+
 public class Ex2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*
+		두 정수를 입력받아 두 정수의 산술 연산 결과를 출력하는 코드를 작성하세요.
+	단 2번째 숫자는 0 이 아님.
 
+	예:
+	두 정수를 입력하세요.
+	12 [엔터]
+	1+2 = 3
+	1-2 = -1
+	1*2 = 2
+	1/2 =0.5
+	1%2=1
+			
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		System.out.println("1정수를 입력해주세요");
+		int num1 = sc.nextInt(); // 정수를 입력 받음
+		System.out.println(num1);
+		
+		
+		System.out.println("2정수를 입력해주세요");
+		int num2 = sc.nextInt(); // 정수를 입력 받음
+		System.out.println(num2);
+		
+		int sum = num1 + num2;
+		int sub = num1 - num2;
+		int mul = num1 * num2;
+		double div =(double) num1 / num2; // 1/2의 결과가 0 이여서, 1.0/2로 바꾸어 0.5가 나오게함
+		int mod = num1 % num2;
+		
+		System.out.println(""+num1+'+'+ num2+ "="+sum);
+		System.out.println(""+num1+'-'+ num2+ "="+sub);
+		System.out.println(""+num1+'*'+ num2+ "="+mul);
+		System.out.println(""+num1+'/'+ num2+ "="+div);
+		System.out.println(""+num1+'%'+ num2+ "="+mod);
+	*/
+	
+		System.out.println("두정수를 입력하세요.");
+		//안내분구 입력
+		Scanner sc=new Scanner(System.in);
+		//두 정수를 입력 받음
+		
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		//두 정수의 산술 연산을 계산
+		
+		//int sum = num1 + num2;
+		int sub = num1 - num2;
+		int mul = num1 * num2;
+		double div =(double) num1 / num2;
+		int mod = num1 % num2;
+		
+		//System.out.println(""+num1+'-'+ num2 + "="+sum);
+		System.out.println(""+num1+'+'+ num2 + "="+(num1 + num2)); // + 연산자가 먼저 수행되기 때문에 + , - 가있을경우 괄호 표시 필요
+		System.out.println(""+num1+'-'+ num2 + "="+sub);
+		System.out.println(""+num1+'*'+ num2 + "="+mul);
+		System.out.println(""+num1+'/'+ num2 + "="+div);
+		//System.out.println(""+num1+'%'+ num2 + "="+mod);
+		
+		System.out.println(""+num1+'%'+ num2 + "="+(num1 % num2));
+		
+		sc.close();
+		
+		
 	}
 
 }
