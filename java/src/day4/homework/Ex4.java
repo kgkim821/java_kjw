da96d1ba6d4c00a4808b9539ed9a417f54701fe9
kgkim821 <kgkim821@naver.com>
2023-06-02 15:19:17 +0900

    Update Ex4.java

--- java/src/day4/homework/Ex4.java
+++ java/src/day4/homework/Ex4.java
@@ -1,9 +1,16 @@
 package day4.homework;
 
+import java.util.Scanner;
+
 public class Ex4 {
 
 	public static void main(String[] args) {
 
+		int num1, num2;
+		
+		Scanner sc = new Scanner (System.in);
+		
+		
 	}
 
 }
