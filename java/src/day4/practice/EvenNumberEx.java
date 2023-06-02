5b84e08a5cce8cf4ae1b96a686e4832a7e7edf0c
kgkim821 <kgkim821@naver.com>
2023-06-02 14:35:16 +0900

    자바 if else 문예제

---
+++ java/src/day4/practice/EvenNumberEx.java
@@ -0,0 +1,11 @@
+package day4.practice;
+
+public class EvenNumberEx {
+
+	public static void main(String[] args) {
+		//홀짝 판별 예제를 조건 연산지를 이요하여 작성
+		int num =4;
+		String result = num %2 == 0 ? "even":"odd";
+		System.out.println(num + "is a "+result+ "number.");
+	}
+}
