088d2f2d19b860d7618bb7e57266ce5c24e3eda5
kgkim821 <kgkim821@naver.com>
2023-06-05 15:12:51 +0900

    5일차 과제

--- java/src/day5/homework/TeamLeaderEx.java
+++ java/src/day5/homework/TeamLeaderEx.java
@@ -1,10 +1,39 @@
 package day5.homework;
 
+import java.util.Scanner;
+
 public class TeamLeaderEx {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*
+		 * 30명의 학생이 있는 반에서 1번부터 연속된 번호를 가진 3명의 학생이 1조가 되도록 나누었다. 이 때 가장 큰 번호인 학생이 조장이 된다.
+학생 번호가 주어졌을 때 조장인지 조원인지 판별하는 코드를 작성하세요.
+(switch문으로)
+input student number : 
+1
+Student number 1 is a member of the team.
+input student number : 
+6
+Student number 6 is the team leader.
 
+		 */
+		int num;
+		int i;
+		
+		Scanner sc = new Scanner(System.in);
+		num = sc.nextInt();
+		
+		num = num%3;
+		switch()
+		{
+		case 0:
+			System.out.println("");
+		case 1:
+			System.out.println("");
+		case 2:
+			System.out.println("");
+		}
+		
 	}
 
 }
--- java/src/day5/homework/UpDownEx.java
+++ java/src/day5/homework/UpDownEx.java
@@ -1,10 +1,31 @@
 package day5.homework;
 
+import java.util.Scanner;
+
 public class UpDownEx {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int num;
+		int min =1 , max =100;
+		int random = (int)(Math.random() * (max-min+1)+min);
+		
+		System.out.println("input positive number : ");
+		Scanner sc = new Scanner(System.in);
+		num = sc.nextInt();
+		
+		while (num != random) {
+			
+			if(num>random) {
+				System.out.println("Down");
+			}else if (num < random) {
+				System.out.println("Up");
+			}
+			num = sc.nextInt();
+		}
+		System.out.println("good!");
+		sc.close();
+		
 	}
 
 }
