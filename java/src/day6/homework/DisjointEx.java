4d02fc1ca6521111d0f410fefc35999efcfb3b57
kgkim821 <kgkim821@naver.com>
2023-06-07 14:48:38 +0900

    자바 6일차 : homework

---
+++ java/src/day6/homework/DisjointEx.java
@@ -0,0 +1,21 @@
+package day6.homework;
+
+import java.util.Scanner;
+
+public class DisjointEx {
+
+	public static void main(String[] args) {
+
+		/* 두정수를 읿력받아 두 정수가 서로소 관계인지 판별하는 코드를 작성하세요
+		 * 
+		 * 
+		 */
+		
+		int num1, num2 , i=1 , dis=0;
+	
+		Scanner sc = new Scanner(System.in);
+		
+		
+	}
+
+}
