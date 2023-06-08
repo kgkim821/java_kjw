89a89bedae671d0d6ea03d20918797d5740deb62
kgkim821 <kgkim821@naver.com>
2023-06-08 21:33:57 +0900

    homework

--- java/src/day7/homework/StarEx5.java
+++ java/src/day7/homework/StarEx5.java
@@ -1,4 +1,4 @@
-package day7.practice;
+package day7.homework;
 
 public class StarEx5 {
 
@@ -29,6 +29,23 @@ public class StarEx5 {
 			}
 			//엔터
 			System.out.println();
+			
 		}
+		for (int i = 1;i<=3;i++) {
+
+			for (int j=1;j<=i+1;j++) {
+				System.out.print(' ');
+			}
+			for (int j=1;j<=4-i;j++) {
+				System.out.print('*');
+			}
+			for (int j=1;j<=3-i;j++) {//j는 1부터 i-1 까지 출력
+				System.out.print('*');
+			}
+			//엔터
+			System.out.println();
+			
+		}
+		
 	}
 }
