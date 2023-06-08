11acf26ed51bd00c1ace429506717027a692dd2d
kgkim821 <kgkim821@naver.com>
2023-06-08 09:15:23 +0900

    자바 6일차 과제 : 서로소 과제

--- java/src/day6/homework/DisjointEx.java
+++ java/src/day6/homework/DisjointEx.java
@@ -7,11 +7,16 @@ public class DisjointEx {
 	public static void main(String[] args) {
 
 		/* 두정수를 읿력받아 두 정수가 서로소 관계인지 판별하는 코드를 작성하세요
-		 * 
-		 * 
+		 	/*
+		 * 반복횠수 : i 는 1부터 num1 까지 1씩 증가
+		 * 규칙성 :i 가 num1의 약수이고 i 가 num2 의 약수이면  i 를 gcd에 저장
+		 * num1을 i로 나누었을때 나머지가 0과 같고
+		 * num2를 i로 나누었을때 나머지가 0과 같다면
+		 * i를 gcd에 저장
+		 * 반복문 졸료 후 : gcd가 1이면 disjoint를 출력하고,
+		 * 아니면 Not disjoint를 출력
 		 */
-		
-		int num1, num2 ,i=1, count = 0, gdc=0 ;
+		int num1, num2 ,i=1, count = 0, gcd=0 ;
 	
 		Scanner sc = new Scanner(System.in);
 		
@@ -19,29 +24,26 @@ public class DisjointEx {
 		num1=sc.nextInt();
 		num2=sc.nextInt();
 		
-		/*for(i=1;i<=num1;i++) {
-			
-				if (num1 % i == 0 && num2 % i ==0 ) {
-					gdc=i;
-					}
-				if(gdc ==1) {
-				System.out.println("ok");
-				} else {
-					System.out.println("no");
-				}
-			}*/
-		while(i<=num1) {
+		/*while(i<=num1) {
 			if(num1 % i == 0 && num2 % i == 0) {
-				gdc = i ;
+				gcd = i ;
 			}
 			i++;
+		}*/
+		
+		for(;i<=num1;i++){
+			if (num1% i ==0 && num2%i==0) {
+				gcd = i;
+			}
 		}
-		if (gdc==1) {
-			System.out.println("ok");
-				
+		
+		
+		if (gcd==1) {
+			System.out.println("Disjoint!");
 		}else {
-			System.out.println("no");
+			System.out.println("Not disjoint!");
 		}
+		//gcd가 1이면 Disjoint를 출력하고, 아니면 Not Disjoint를 출력
+		sc.close();
 		}
 }
-
