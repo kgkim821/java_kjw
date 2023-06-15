32396bbcb756b99ecb9135f5382ff2cd55c1d3f2
kgkim821 <kgkim821@naver.com>
2023-06-15 23:32:52 +0900

    수정

--- java/src/day12/homework/LottoEx.java
+++ java/src/day12/homework/LottoEx.java
@@ -1,9 +1,44 @@
 package day12.homework;
 
-public class LottoEx {
+import java.util.ArrayList;
+import java.util.Scanner;
 
-	public static void main(String[] args) {
+import array.Array;
 
-	}
+ public class LottoEx{
+	 public static void main(String[] args) {
+	
+		 System.out.println("input number :");
+		 Scanner sc = new Scanner(System.in);
+		 
+		 int[] usernum = new int [6];
+		 
+		 for(int i =0;i<6;i++) {
+			 usernum[i] = sc.nextInt();
+		 }
+	 
+		 
+		 int min=1 , max=45 ;
+		 int [] com  = new int[6];
+		 Array.createRandomArray(min, max, com);
+		 System.out.println(com[0]);
+		 System.out.println(com[1]);
+		 System.out.println(com[2]);
+	 }
+ }
+	/*
+	 * 로또 번호를 생성하고, 사용자가 번호를 입력하면 몇등인지 출력하는 코드를 작성하세요.
+로또 번호는 1~45
+(package : day12.homework, file : LottoEx)
+1등 : 번호 6개
+2등 : 번호 5개 + 보너스
+3등 : 번호 5개
+4등 : 번호 4개
+5등 : 번호 3개
+number: [1, 3, 40, 23, 24, 45]
+bonus : 2
+user  : 1 2 3 4 5 6
+losing ticket!
 
-}
+	 */
+	
--- java/src/day12/practice/Word.java
+++ java/src/day12/practice/Word.java
@@ -4,69 +4,66 @@ public class Word{
 	//멤버 변수
 	private String title;
 	private String meaning[];
-	private int meaningCount;//뜻이 저장된 갯수
+	private int meaningCount;
 	//생성자
-	public Word(String title,String meaning) {
+	public Word(String title, String meaning) {
 		this.title = title;
-		this.meaning = new String[5];// 기본 5개
+		this.meaning = new String[5]; //기본 5개
 		this.meaning[0] = meaning;
 		meaningCount++;
 	}
 	public Word(String title) {
 		this.title = title;
-		this.meaning = new String[5];
+		this.meaning = new String[5]; //기본 5개
 	}
 	public Word(Word w) {
 		this.title = w.title;
-		this.meaning = new String[5];
-		for (int i =0; i<w.meaningCount;i++) {
-			meaning[i] = w.meaning[i];
+		this.meaning = new String[5]; //기본 5개
+		for(int i = 0; i<w.meaningCount; i++) {
+			this.meaning[i] = w.meaning[i];
 		}
 		meaningCount = w.meaningCount;
 	}
 	//메서드
-/**단어와 뜻을 출력하는 메서드
- * 매개변수 : 없음
- * 리턴타입 : 없음 => void
- * 메서드명 : print
- */
+	/**단어와 뜻을 출력하는 메서드
+	 * 매개변수 : 없음
+	 * 리턴타입 : 없음 => void
+	 * 메서드명 : print
+	 */
 	public void print() {
-		System.out.println("word :" + title);
+		System.out.println("word : " + title);
 		System.out.println("meaning : ");
-		for (int i=0; i < meaningCount; i++) {
-				System.out.println(i+1+". " + meaning[i]);
-			}
+		for(int i = 0; i<meaningCount; i++) {
+			System.out.println(i+1+". " + meaning[i]);
 		}
+	}
 	/**뜻을 추가하는 메서드
 	 * 매개변수 : 추가될 뜻 => String meaning
-	 * 리턴타입 : 없음
+	 * 리턴타입 : 없음 => void
 	 * 메서드명 : addMeaning
-	 * 
 	 */
 	public void addMeaning(String meaning) {
-		//뜻이다 찼으면 다찼다 출력하고 종료
-		if(meaningCount== this.meaning.length) {
-			System.out.println("It's all failed.");
+		//뜻이 다 찼으면 다 찼다고 출력하고 종료
+		if(meaningCount == this.meaning.length) {
+			System.out.println("It's all filled.");
 			return;
 		}
 		this.meaning[meaningCount] = meaning;
 		meaningCount++;
 	}
-
-	/**뜻을 제거하는 매서드
-	 * 매개변수 : 제거할 뜻의 번호 =>  int num
-	 * 리턴타입 : 없음 =>void
+	/**뜻을 제거하는 메서드
+	 * 매개변수 : 제거할 뜻의 번호 => int num
+	 * 리턴타입 : 없음 => void
 	 * 메서드명 : removeMeaning
-	 * 
 	 */
 	public void removeMeaning(int num) {
-		if(num > meaningCount|| num < 1) {
+		if(num > meaningCount || num < 1) {
 			System.out.println("I can't work.");
 			return;
 		}
-		//num -1 번지부터 하나씩 당겨와서 덮어쓰기함.
-		for(int i =num-1; i <meaningCount-1;i++) {
-			meaning[i]= meaning[i+1];
+		//num-1번지부터 하나씩 당겨와서 덮어쓰기함.
+		for(int i = num-1; i < meaningCount-1; i++) {
+			meaning[i] = meaning[i+1];
 		}
 		//마지막에 쓸모없는 데이터를 지움
 		meaning[meaningCount-1] = null;
