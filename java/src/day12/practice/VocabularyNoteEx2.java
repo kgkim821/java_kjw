cb7216cb8158dad793a683267223ebaa79136fa8
kgkim821 <kgkim821@naver.com>
2023-06-16 12:36:23 +0900

    자바:  단어장 예제

--- java/src/day12/practice/VocabularyNoteEx2.java
+++ java/src/day12/practice/VocabularyNoteEx2.java
@@ -7,9 +7,36 @@ public class VocabularyNoteEx2 {
 				new Word("abc"),
 				new Word("def","def is define")
 		};
-
+		
+		
+		
 		VocabularyNote note = new VocabularyNote(wordList);
+		//note.print();
+	
+		Word word = new Word("a", "a");
+		note.insert(word);
+		//note.print();
+		note.insert("b","b is b");
+		//note.print();
+		
+		//note.delete("c"); 
+		note.delete("a");
+		//note.print();
+		
+		//note.serch("a");
+		//note.serch("b");
+		
+		note.insert("b", "b");
 		note.print();
+		
+		note.updateMeaning("b", 2, "B");
+		note.updateMeaning("b", 3, "B"); //Wrong number
+		note.print();
+		
+		note.updateTitle("b", "B");
+		note.updateTitle("b", "B");
+		note.print();
+		
 	}
 
 }
