3ea851ea662e343f3dc130893b81219a3a2f507c
kgkim821 <kgkim821@naver.com>
2023-06-18 19:24:59 +0900

    homework

--- java/src/day13/Sort/ArraySortEx.java
+++ java/src/day13/Sort/ArraySortEx.java
@@ -1,5 +1,6 @@
 package day13.Sort;
 
+import java.util.Arrays;
 import java.util.Comparator;
 
 import array.Array;
@@ -7,27 +8,24 @@ import array.Array;
 public class ArraySortEx {
 
 	public static void main(String[] args) {
-
-		//버블 정렬 예제
-		int arr[] = { 1, 4, 7, 9, 2, 8, 3, 6};
+		// 버블 정렬 예제
+		int arr[] = {1, 4, 7, 9, 2, 8, 3, 6};
 		Array.printArray(arr);
-		for(int i =0; i< arr.length-1; i++) {
-			for (int j = 0 ; j<arr.length-1-i;j++) {
+		for(int i = 0; i<arr.length-1; i++) {
+			for(int j = 0; j<arr.length-1-i; j++) {
 				if(arr[j] > arr[j+1]) {
 					int tmp = arr[j];
-					arr[j]= arr[j+1];
-					arr[j+1]=tmp;
+					arr[j] = arr[j+1];
+					arr[j+1] = tmp;
 				}
 			}
 			Array.printArray(arr);
 		}
-		Integer arr2[] = { 1,4,7,9,2,8,3,6};
-		
-	
-		//Array.sort(arr2,Comparator.reverseOrder());
+		Integer arr2[] = {1, 4, 7, 9, 2, 8, 3, 6};
+		//Arrays.sort(arr2);//오름차순 
+		Arrays.sort(arr2, Comparator.reverseOrder());//내림차순
 		for(Integer tmp : arr2) {
-			System.out.println(tmp + " ");
-		
+			System.out.print(tmp + " ");
 		}
 	}
 }
--- java/src/day13/homework/VocabularyNoteEx.java
+++ java/src/day13/homework/VocabularyNoteEx.java
@@ -1,10 +1,122 @@
 package day13.homework;
 
+import java.util.Scanner;
+
+
+import day12.practice.VocabularyNote;
+import day12.practice.Word;
+import lombok.Data;
+
 public class VocabularyNoteEx {
 
+
+		/*
+		 * 영어 단어장 프로그램을 구현하세요.
+Word, VocabularyNote 클래스 이용
+(package : day13.homework, file : VocabularyNoteEx)
+메뉴
+단어 추가
+단어 삭제
+단어 수정
+단어 출력
+프로그램 종료
+메뉴 선택 : 3
+서브 메뉴
+단어 수정
+뜻 수정
+뜻 삭제
+서브 메뉴 선택 : 1
+수정할 단어 : a
+수정될 단어 : A
+수정에 성공했습니다.
+
+		 */
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+        int num,num2;
+        //VocabularyNote voca = new VocabularyNote();
+        Word wordList[] = {
+				new Word("abc"),
+				new Word("def","def is define")
+		};
+      		
+		VocabularyNote note = new VocabularyNote(wordList);
+		
+		
+		
+		do {
+            System.out.println("Menu:");
+            System.out.println("1. Add a word");
+            System.out.println("2. Delete a word");
+            System.out.println("3. Modify a word");
+            System.out.println("4. Output all words");
+            System.out.println("5. Exit the program");
+            System.out.print("Enter your choice: ");
+            num = sc.nextInt();
+
+            switch (num) {
+                case 1:
+                	System.out.print("input word :(ex : (word) (meaning)): ");
+                	String str1 = sc.next();
+                	String str2 = sc.nextLine();
+                	
+                	note.insert(str1, str2);
+                	break;
+                case 2:
+                	System.out.println("input word :");
+                	String str3 = sc.next();
+                	
+                	note.delete(str3);
+                	
+                    break;
+                case 3:
+                	 System.out.println("1. Change a word");
+                     System.out.println("2. Change a meaning");
+                     System.out.println("3. delete a meaning");
+                     System.out.println("Input number : ");
+
+                     int num1 = sc.nextInt();
+                     if(num1 == 1) {
+                    	 System.out.println("input title and change title :");
+                    	 String title = sc.next();
+                    	 String chgtitle = sc.nextLine();
+                    	 note.updateTitle(title, chgtitle);
+                    	 break;
+                    	 
+                     }else if (num1 ==2 ) {
+                    	 System.out.println("input title and change meaning , title , meaning");
+                    	 
+                    	 
+                    	 
+            
+                    	 
+                    	 
+                    	 
+                     }else if (num1 ==3) {
+                    	 
+                    	 
+                     }
+                     
+                    break;
+                case 4:
+                	
+                	note.print();
+                	
+                    break;
+                case 5:
+                    System.out.println("Exiting the program");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+		
+		}while (num != 5 );
+		sc.close();
 	}
-
 }
