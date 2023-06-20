8f8d5376879d8a451dee97d766b09c7bc2282ac4
kgkim821 <kgkim821@naver.com>
2023-06-20 15:01:57 +0900

    15일차  쇼핑 예제

---
+++ java/src/day15/practice/shop/run/Run.java
@@ -0,0 +1,15 @@
+package day15.practice.shop.run;
+
+import day15.practice.controller.ShopManger;
+
+public class Run {
+
+	public static void main(String[] args) {
+		// ShopMaager 를 이용하여 제품을 관리하는 프로그램 실행
+
+		//ms949
+		ShopManger shopmanager1 = new ShopManger();
+		
+	}
+
+}
