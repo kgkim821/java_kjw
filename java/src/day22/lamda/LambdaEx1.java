1ff7f6d957487a37cc0af786389843fdc862237a
kgkim821 <kgkim821@naver.com>
2023-06-29 15:04:10 +0900

    22일차 lamda

---
+++ java/src/day22/lamda/LambdaEx1.java
@@ -0,0 +1,35 @@
+package day22.lamda;
+
+public class LambdaEx1 {
+
+	public static void main(String[] args) {
+
+	InterfaceA a = new InterfaceA() {
+		
+		@Override
+		public int getMax(int num1, int num2) {
+			return num1 >= num2 ? num1 : num2;
+			
+		}
+	};
+	System.out.println(a.getMax(10, 20));
+	
+		InterfaceA b= (num1,num2)->{
+			return num1 >= num2 ? num1 : num2;
+		};
+		
+		System.out.println(b.getMax(10, 20));
+	}
+
+	//InterfaceA c= (num1,num2)->num1 >= num2 ? num1 : num2;
+
+	
+	//System.out.println(c.getMax(10, 20));
+
+}
+
+interface InterfaceA{
+
+	int getMax(int num1, int num2);
+
+}
