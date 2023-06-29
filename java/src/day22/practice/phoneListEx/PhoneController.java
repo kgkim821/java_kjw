0d5a1b503f914eec067e6caf7a955ae9aa01132d
kgkim821 <kgkim821@naver.com>
2023-06-30 00:49:13 +0900

    과제

--- java/src/day22/practice/phoneListEx/PhoneController.java
+++ java/src/day22/practice/phoneListEx/PhoneController.java
@@ -1,12 +1,14 @@
 package day22.practice.phoneListEx;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class PhoneController {
 
 	
 	private Scanner sc = new Scanner(System.in);
-	
+	private ArrayList<Integer> addressList = new ArrayList<>();
+
 	
 	public void run () {
 		
@@ -25,11 +27,11 @@ public class PhoneController {
 	}
 	
 	private void printMenu() {
-		System.out.println("menu");
-		System.out.println("1.address input :");
-		System.out.println("2.address change");
-		System.out.println("3.address search");
-		System.out.println("4.exit program");
+		System.out.println("Menu");
+		System.out.println("1.Address input :");
+		System.out.println("2.Address change");
+		System.out.println("3.Address search");
+		System.out.println("4.Exit program");
 		System.out.println("intput number");
 	}
 
@@ -54,19 +56,39 @@ public class PhoneController {
 	}
 
 	private void inputAddress() {
-		System.out.println("input address :");
-		String address = sc.next();
-		
-		
-		
+	    System.out.print("Enter address: ");
+	    int address = sc.nextInt();
+
+	    addressList.add(address);
+	    System.out.println("Address added: " + address);
 	}
+
 	private void changeAddress() {
-		
-		
+	    System.out.print("Enter the index of the address to change: ");
+	    int index = sc.nextInt();
+
+	    if (index >= 0 && index < addressList.size()) {
+	        System.out.print("Enter the new address: ");
+	        int newAddress = sc.nextInt();
+
+	        addressList.set(index, newAddress);
+	        System.out.println("Address at index " + index + " changed to " + newAddress);
+	    } else {
+	        System.out.println("Invalid index. No address found.");
+	    }
 	}
+
 	private void searchAddress() {
-		
-		
+	    System.out.print("Enter the address to search for: ");
+	    int searchAddress = sc.nextInt();
+
+	    int index = addressList.indexOf(searchAddress);
+
+	    if (index != -1) {
+	        System.out.println("Address found at index " + index);
+	    } else {
+	        System.out.println("Address not found.");
+	    }
 	}
 
 	
--- java/src/day22/practice/phoneListEx/Run.java
+++ java/src/day22/practice/phoneListEx/Run.java
@@ -2,4 +2,12 @@ package day22.practice.phoneListEx;
 
 public class Run {
 
+	
+	public static void main (String[] args) {
+		
+		
+		PhoneController pc = new PhoneController();
+		pc.run();
+				
+	}
 }
