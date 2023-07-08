29a8cf9a560f6161d69181deb1438852d7d981c2
kgkim821 <kgkim821@naver.com>
2023-07-08 16:30:03 +0900

    야구게임 예제

--- java/src/day28/baseballGame/run/Run.java
+++ java/src/day28/baseballGame/run/Run.java
@@ -1,5 +1,12 @@
 package day28.baseballGame.run;
 
+import day28.baseballGame.controller.GameController;
+
 public class Run {
+	public static void main(String[] args) {
+		GameController gc = new GameController();
+		gc.run();
+
+	}
 
 }
